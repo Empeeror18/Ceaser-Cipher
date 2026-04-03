81245a8ec2a016cc3a5bb88817b7ffb95fc87405
Empeeror18 <saryal878@gmail.com>
2026-04-03 09:59:07 +0545

    Added final touch

--- main.py
+++ main.py
@@ -1,7 +1,55 @@
-encrypt():   
-    text = input("Enter the text to encrypt: ")
-    shift = int(input("Enter the shift value (1-25): "))
+alphabet_length = 26
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+run_program = True
+
+print("\nWelcome to the Caesar Cipher Program!\n")
+
+def encrypt(text, key):
     encrypted_text = ""
     for char in text:
-        if char.find(" ") != -1:
+        if char in alphabets:
+            char_index = alphabets.find(char)
+            new_index = (char_index + key) % alphabet_length
+            encrypted_text += alphabets[new_index]
+        else:
             encrypted_text += char
+    print("Encrypted text:", encrypted_text)
+
+def decrypt(text, key):
+    decrypted_text = ""
+    for char in text:
+        if char in alphabets:
+            char_index = alphabets.find(char)
+            new_index = (char_index - key) % alphabet_length
+            decrypted_text += alphabets[new_index]
+        else:
+            decrypted_text += char
+    print("Decrypted text:", decrypted_text)
+
+while run_program:
+    Choice = input("Do you want to encrypt or decrypt? (e/d): ").lower()
+    text = input("Enter the text: ").lower()
+
+    while True:
+        try:
+            shift = int(input("Enter the shift value (1-25): "))
+            break
+        except ValueError:
+            print("Invalid shift value. Please enter a number between 1 and 25.")
+
+    if Choice == "e":
+        print("\nEncrypting...")
+        print()
+        encrypt(text, shift)
+    
+    elif Choice == "d":
+        print("\nDecrypting...")
+        print()
+        decrypt(text, shift)
+
+    New_choice = input("\nDo you want to continue?(y/n):").lower()
+    print()
+
+    if New_choice == "n" or New_choice == "no":
+        run_program = False
+print("Thank you for using the Caesar Cipher Program! Goodbye!")
